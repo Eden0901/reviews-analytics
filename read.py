ea4728eb7fb2e40614955862f3278753e0423604
Eden Li <zhaosl0901@gmail.com>
2020-12-25 15:10:11 -0600

    add filtering

--- read.py
+++ read.py
@@ -14,3 +14,11 @@ for d in data:
 	
 print('留言的平均长度为', sum_len/len(data))
 
+#筛选
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '笔留言长度小于100' )
+print(new[0])
+print(new[1])
